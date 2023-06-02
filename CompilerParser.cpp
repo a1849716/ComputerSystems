84de5ce854e837478663b6b927df8c6acd68e615
Yuqi Xiao <a1849716@adelaide.edu.au>
2023-06-02 18:56:24 +0930

    save

--- CompilerParser.cpp
+++ CompilerParser.cpp
@@ -1,5 +1,7 @@
 #include "CompilerParser.h"
 
+#include <iostream>
+
 using namespace std;
 
 /**
@@ -8,7 +10,7 @@ using namespace std;
  */
 CompilerParser::CompilerParser(std::list<Token*> tokens) {
   this->tokenList = tokens;
-  currentToken = tokens.front();
+  currentToken = current();
 }
 
 /**
